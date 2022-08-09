a4d45d7b92da24246041304508702540a5129b37
Tobias Maymo <tobiasmmaymo@gmail.com>
2022-08-08 23:52:48 -0300

    correccion

--- js/app.js
+++ js/app.js
@@ -19,22 +19,17 @@ const Album = [
     {nombre: '2COOL4SKOOL', precio: 3500, img: './images/2c4s.jpg', id: 18, cantidad: 1},
 ];
 document.addEventListener('DOMContentLoaded', () => {
-    fetchData()
+    cargarAlbum()
 })
-const fetchData = async () => {
-  try {
-    const res = await fetch('./js/data.json')
-    const data = await res.json()
-    console.log(data)
-  } catch (error) {
-    console.log(error)
-  }
-}
+
 const contenedoralbumnes = document.getElementById('contenedor-productos')
 const contenedorCarrito = document.getElementById('carrito-contenedor')
 const botonVaciar = document.getElementById('vaciar-carrito')
 const contadorCarrito = document.getElementById('contadorCarrito')
 const precioTotal = document.getElementById('precioTotal')
+const comprarBoton = document.getElementById('comprar-carrito')
+const div = document.createElement('div')
+
 
 let carrito = []
 
@@ -45,103 +40,152 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 })
 
+const cargarAlbum = async () => {
+  try {
+    const respuesta = await fetch('./js/data.json')
+    const data = await respuesta.json()
+    ponerAlbum(data)
+    
+
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 botonVaciar.addEventListener('click', () => {
-    const swalWithBootstrapButtons = Swal.mixin({
-        customClass: {
-          confirmButton: 'btn btn-success',
-          cancelButton: 'btn btn-danger'
-        },
-        buttonsStyling: false
-      })
-      
-      swalWithBootstrapButtons.fire({
-        title: 'Estas Seguro?',
-        text: "No Podras Recuperar La Compra Luego",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonText: 'Elimina!',
-        cancelButtonText: 'Cancela!',
-        reverseButtons: true
-      }).then((result) => {
-        if (result.isConfirmed) {
-          swalWithBootstrapButtons.fire(
-            'Borrado!',
-            'Tu Compra Fue Borrada!',
-            'success'
-          )
-            carrito.length = 0
-        actualizarCarrito()
-        } else if (
-          result.dismiss === Swal.DismissReason.cancel
-        ) {
-          swalWithBootstrapButtons.fire(
-            'Cancelado',
-            'Tu Compra Esta A Salvo :)',
-            'error'
-          )
-        }
-      })
-})
-
-Album.forEach((producto) => {
-    const div = document.createElement('div')
-    div.classList.add('producto')
-    div.innerHTML = `
-    <div class="col">
-        <div class="card shadow">
-            <img src=${producto.img} alt=${producto.nombre} class="bd-placeholder-img card-img-top" width="100%" height="100%">
-            <div class="card-body">
-                <p class="card-text">${producto.nombre}</p>
-                <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                        <button class="boton-agregar" id="agregar${producto.id}">Comprar</button>
-                    </div>
-                    <small class="text-muted">$${producto.precio}</small>
-                </div>
-            </div>
-        </div>
-    </div>
-`
-    contenedoralbumnes.appendChild(div)
-
-    const boton = document.getElementById(`agregar${producto.id}`)
-
-    boton.addEventListener('click', () => {
-        agregarAlCarrito(producto.id)
+  const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: 'btn btn-success',
+        cancelButton: 'btn btn-danger'
+      },
+      buttonsStyling: false
     })
-
+    
+    swalWithBootstrapButtons.fire({
+      title: 'Estas Seguro?',
+      text: "No Podras Recuperar La Compra Luego",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Elimina!',
+      cancelButtonText: 'Cancela!',
+      reverseButtons: true
+    }).then((result) => {
+      if (result.isConfirmed) {
+        swalWithBootstrapButtons.fire(
+          'Borrado!',
+          'Tu Compra Fue Borrada!',
+          'success'
+        )
+          carrito.length = 0
+      actualizarCarrito()
+      } else if (
+        result.dismiss === Swal.DismissReason.cancel
+      ) {
+        swalWithBootstrapButtons.fire(
+          'Cancelado',
+          'Tu Compra Esta A Salvo :)',
+          'error'
+        )
+      }
+    })
 })
+
+const ponerAlbum = data => {
+    data.forEach(producto => {
+        const div = document.createElement('div')
+        div.classList.add('producto')
+          div.innerHTML = 
+            `<div class="col">
+              <div class="card shadow">
+                <img src=${producto.img} alt=${producto.nombre} class="bd-placeholder-img card-img-top" width="100%" height="100%">
+                  <div class="card-body">
+                    <p class="card-text">${producto.nombre}</p>
+                      <div class="d-flex justify-content-between align-items-center">
+                        <div class="btn-group">
+                          <button class="boton-agregar" id="agregar${producto.id}">Comprar</button>
+                        </div>
+                        <small class="text-muted">$${producto.precio}</small>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+            </div>`
+            contenedoralbumnes.appendChild(div)
+            
+            const boton = document.getElementById(`agregar${producto.id}`)
+            boton.addEventListener('click', () => {
+            agregarAlCarrito(producto.id)
+        })
+})}
 
 const agregarAlCarrito = (prodId) =>{
-    const existe = carrito.some (prod => prod.id === prodId)
-    if (existe){
-        const prod = carrito.map (prod => {
-            if (prod.id === prodId){
-                prod.cantidad++
-            }
-        })
-    } else {
-        const item = Album.find((prod) => prod.id === prodId)
-        carrito.push(item)
-    }
-    actualizarCarrito()
-    const Toast = Swal.mixin({
-        toast: true,
-        position: 'top-start',
-        showConfirmButton: false,
-        timer: 3000,
-        timerProgressBar: false,
-        didOpen: (toast) => {
-          toast.addEventListener('mouseenter', Swal.stopTimer)
-          toast.addEventListener('mouseleave', Swal.resumeTimer)
-        }
+  const existe = carrito.some (prod => prod.id === prodId)
+  if (existe){
+      const prod = carrito.map (prod => {
+          if (prod.id === prodId){
+              prod.cantidad++
+          }
       })
-      
-      Toast.fire({
-        icon: 'success',
-        title: 'Agregado Al Carrito Exitosamente'
-      })
+  } else {
+      const item = Album.find((prod) => prod.id === prodId)
+      carrito.push(item)
+  }
+  actualizarCarrito()
+  const Toast = Swal.mixin({
+      toast: true,
+      position: 'top-start',
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: false,
+      didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+      }
+    })
+    
+    Toast.fire({
+      icon: 'success',
+      title: 'Agregado Al Carrito Exitosamente'
+    })
 }
+
+comprarBoton.addEventListener('click', () => {
+  const swalWithBootstrapButtons = Swal.mixin({
+    customClass: {
+      confirmButton: 'btn btn-success',
+      cancelButton: 'btn btn-danger'
+    },
+    buttonsStyling: false
+  })
+  
+  swalWithBootstrapButtons.fire({
+    title: 'Estas Seguro?',
+    text: "",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonText: 'Comprar',
+    cancelButtonText: 'Cancelar',
+    reverseButtons: true
+  }).then((result) => {
+    if (result.isConfirmed) {
+      swalWithBootstrapButtons.fire(
+        'Comprado!',
+        'Que disfrutes de tu compra!',
+        'success'
+      )
+        carrito.length = 0
+    actualizarCarrito()
+    } else if (
+      result.dismiss === Swal.DismissReason.cancel
+    ) {
+      swalWithBootstrapButtons.fire(
+        'Cancelado',
+        'Vuelve mas tarde! T-T',
+        'error'
+      )
+    }
+  })
+});
 
 const eliminarDelCarrito = (prodId) => {
     const item = carrito.find((prod) => prod.id === prodId)
